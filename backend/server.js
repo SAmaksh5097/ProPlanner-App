b88d8fea36f7c55e184e8a059eb2293464f478eb
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-02-27 23:01:38 +0530

    db schema made

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./src/config/db');
 
+// connect to database
+connectDB().then(()=>{
+  console.log("DB connected");
+}).catch((err)=>{
+  console.log("error occured in db connection");
+})
+
+app.get('/',(req,res)=>{
+  res.json({msg:"HELLo user"})
+})
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
