26aa059148bd3076ecfd71eeca6cdf35a43c1934
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-02-27 19:58:06 +0530

    changes in homepage

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -19,9 +19,9 @@ const ThemeToggle = () => {
   const [spin,setSpin] = useState(false)
 
   return (
-    <div>
+    <div className='flex'>
         <button className="rounded  border-gray-400 dark:border-gray-400 cursor-pointer shadow-mdhover:shadow-gray-600 transition-all ease-in-out" onClick={toggle}>
-            {theme==="light"?<Moon className={`h-8 w-8 ${spin?"animate-spin":""} `}/>:<Sun className={`h-8 w-8 ${spin?"animate-spin":""} `}/>}
+            {theme==="light"?<Moon className={`h-6 w-6 ${spin?"animate-spin":""} `}/>:<Sun className={`h-6 w-6 ${spin?"animate-spin":""} `}/>}
         </button>
 
       
