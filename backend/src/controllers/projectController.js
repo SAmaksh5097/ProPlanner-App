433bd881b0ac17f2f662d340b3d2c18b879ab813
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-02-28 14:30:54 +0530

    backend completed

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -75,11 +75,56 @@ async function getProjectById(req,res){
 
 }
 
+async function toogleTask(req,res){
+    try{
+        const {projectId, dayNumber, taskTitle} = req.body;
+        const userId = req.auth.userId;
+
+        const project = await projectModel.findOne({_id:projectId, userId});
+        if(!project){
+            return res.status(404).json({error:"Project not found"});
+        }
+        const day = project.roadmap.find(d => d.dayNumber === dayNumber);
+        const task = day.tasks.find(t => t.title === taskTitle);
+        if(task){
+            task.completed = !task.completed;
+
+            const allTasks = project.roadmap.flatMap(d => d.tasks);
+            const completedTasks = allTasks.filter(t => t.completed).length;
+            project.progress = Math.round((completedTasks / allTasks.length) * 100);
+
+            await project.save();
+            res.json(project);
+        }
+        else{
+            res.status(404).json({error:"Task not found"});
+        }
+    } catch(error){
+        res.status(500).json({error:"Server error"});
+    }
+}
+
+async function deleteProject(req,res){
+    try {
+        const { id } = req.params;
+        const userId = req.auth.userId;
+
+        const deleted = await projectModel.findOneAndDelete({ _id: id, userId });
+        if (!deleted) return res.status(404).json({ error: "Project not found" });
+
+        res.json({ message: "Project deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Server error during deletion" });
+    }
+
+}
+
+
 
 
 
 module.exports = {
-    createProject,getProjectById,getUserProjects
+    createProject,getProjectById,getUserProjects, deleteProject, toogleTask
     
 
 }
